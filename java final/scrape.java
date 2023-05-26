a9b53cd25df11ce1c58ba34ec23c8b4de0b8286b
Josh Angeles <72431498+joshangeless@users.noreply.github.com>
2023-05-25 22:18:53 -0400

    added more

--- java final/scrape.java
+++ java final/scrape.java
@@ -1,4 +1,6 @@
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -6,35 +8,41 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class scrape {
-    public static void main(String[] args) {
-        final String url = "https://www2.hm.com/en_us/men/new-arrivals/view-all.html";
-        ArrayList<String> productsURL = new ArrayList<String>();
-        ArrayList<String> newProducts = new ArrayList<String>();
+    final String url = "https://www2.hm.com/en_us/men/new-arrivals/view-all.html";
+    ArrayList<String> products = new ArrayList<String>();
 
-        try {
-            final Document doc = Jsoup.connect(url).get();
-
-            for (Element row : doc.select("div.item-details")) {
-                System.out.println(row.select("h3.item-heading").text() + " " + row.select("strong.item-price").text());
-            }
-
-            Elements links = doc.select("div.image-container a");
-            for (Element link : links) {
-                String absUrl = link.absUrl("href");
-                System.out.println(absUrl);
-                productsURL.add(absUrl);
-            }
-/* Gonna try to find a way to monitor and see if a new product has been added by comparing arraylists.
-            boolean bool = productsURL.equals(newProducts);
-            if (bool == true) {
-                System.out.println(bool);
-            } else {
-                System.out.println(newProducts);
-            }
- */
-        } catch (Exception ex) {
-            System.out.println(ex);
+    void productInfo() throws IOException {
+        Document doc = Jsoup.connect(url).get();
+        slowPrint("Scrapping H&M...");
+        for (Element row : doc.select("div.item-details")) {
+            slowPrint("\n" + row.select("h3.item-heading").text() + " " + row.select("strong.item-price").text());
         }
+    }
 
+    public ArrayList<String> productLinks() throws IOException {
+        Document doc = Jsoup.connect(url).get();
+        Elements links = doc.select("div.image-container a");
+        for (Element link : links) {
+            String absUrl = link.absUrl("href");
+            products.add(absUrl + "\n");
+        }
+        products.add(0, null);
+        return products;
+    }
+
+    public ArrayList<String> getStringList() {
+        return products;
+    }
+
+    public static void slowPrint(String output) {
+        for (int i = 0; i < output.length(); i++) {
+            char c = output.charAt(i);
+            System.out.print(c);
+            try {
+                TimeUnit.MILLISECONDS.sleep(5);
+            } catch (Exception e) {
+
+            }
+        }
     }
 }
