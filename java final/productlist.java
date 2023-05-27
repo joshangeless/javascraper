9428c93a66f36b994ebf568b3287e2f14530141b
Josh Angeles <72431498+joshangeless@users.noreply.github.com>
2023-05-26 23:15:14 -0400

    FINAL FINAL COMMIT

--- java final/productlist.java
+++ java final/productlist.java
@@ -12,15 +12,16 @@ public class productlist {
 
     public ArrayList<String> productLinks() throws IOException {
         Document doc = Jsoup.connect(url).get();
-        Elements links = doc.select("div.image-container a");
-        for (Element link : links) {
+        Elements links = doc.select("div.image-container a"); // Finding all the links on the page and boiling it down to the product links.
+        for (Element link : links) { // For loop to add them all to an array.
             String absUrl = link.absUrl("href");
             products.add(absUrl + "\n");
         }
-        products.add(0, null);
+        products.add(0, null); // Adding a null value to the zero index to shift the array up by one.
         return products;
     }
 
+    // just another method to return products.
     public ArrayList<String> getStringList() {
         return products;
     }
